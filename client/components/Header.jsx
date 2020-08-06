a299041bf47c31644311692876e38e1261b732eb
matchashiba <recr38@gmail.com>
2020-08-06 20:46:29 +0000

    Imported Nav into Header.jsx Created Nav.Link for each navbar item

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function Header(props) {
   return (
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand className="brand"><i className="fa fa-paw" /> Redwulf</Navbar.Brand>
+      <Nav>
+        <Nav.Link href="#About">About</Nav.Link>
+        <Nav.Link href="#Work">Gallery</Nav.Link>
+        <Nav.Link href="#Commission">Commissions</Nav.Link>
+        <Nav.Link href="#Contact">Contact Me</Nav.Link>
+      </Nav>
     </Navbar>
   );
 }
