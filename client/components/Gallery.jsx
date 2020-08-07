3943c18d51f98fb27bbf7c6e77454a57661c8766
matchashiba <recr38@gmail.com>
2020-08-07 21:51:32 +0000

    Adjusting responsiveness WIP

--- client/components/Gallery.jsx
+++ client/components/Gallery.jsx
@@ -5,37 +5,25 @@ class Gallery extends React.Component {
   render() {
     return (
       <Carousel>
-        <Carousel.Item>
+        <Carousel.Item className="art-slide">
           <img
             src="./assets/xmascard2019.jpeg"
             className="d-block w-100"
             alt="Huntington Holiday Card" />
-          <Carousel.Caption>
-            <h3>Huntington Holiday 2019</h3>
-            <p>Artwork for my 2019 holiday card</p>
-          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="art-slide">
           <img
             src="./assets/LeoComputer.png"
             className="d-block w-100"
             alt="Fox on computer"
           />
-          <Carousel.Caption>
-            <h3>Tinkering</h3>
-            <p>Digital commission made with Medibang</p>
-          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="art-slide">
           <img
             src="./assets/TawnyRoom.png"
             className="d-block w-100"
             alt="Trying on collar in room"
           />
-          <Carousel.Caption>
-            <h3>My Safe Space</h3>
-            <p>Digital commission made with Medibang</p>
-          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     );
