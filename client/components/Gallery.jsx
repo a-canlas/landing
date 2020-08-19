39c0ac1c8f7034130607d85afc3ec7325723a25e
matchashiba <recr38@gmail.com>
2020-08-19 20:45:47 +0000

    Changed name and id values to gallery in Gallery

--- client/components/Gallery.jsx
+++ client/components/Gallery.jsx
@@ -4,7 +4,7 @@ import Carousel from 'react-bootstrap/Carousel';
 class Gallery extends React.Component {
   render() {
     return (
-      <Carousel id="work" name="work">
+      <Carousel id="gallery" name="gallery">
         <Carousel.Item>
           <picture>
             <source srcSet="./assets/holiday-l.png" media="(min-width: 1400px)"/>
